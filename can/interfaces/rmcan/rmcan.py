3c9df6b51eefdb41aadc66a709de2d3761b00bd9
Peter Kessen <p.kessen@kessen-peter.de>
2025-02-21 17:23:04 +0100

    Implement basic connect

--- can/interfaces/rmcan/rmcan.py
+++ can/interfaces/rmcan/rmcan.py
@@ -4,17 +4,35 @@ see https://manuals.plus/proemion/byte-command-protocol-binary-commands-manual
 """
 
 import logging
+import socket
+import time
 
+from collections import deque
 import can
 
 log = logging.getLogger(__name__)
 
 
 class RmcanBus(can.BusABC):
-    def __init__(self, **kwargs):
+    def __init__(self, channel=None, host="localhost", port=30000, **kwargs):
         """Connects to a CAN bus served by Proemion can interface.
         """
-        super().__init__(**kwargs)
+        self.__host = host
+        self.__port = port
+
+
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.__message_buffer = deque()
+        self.__receive_buffer = ""  # i know string is not the most efficient here
+
+        self.__socket.connect((self.__host, self.__port))
+
+        log.info(
+            f"RmcanBus: connected with address {self.__socket.getsockname()}"
+        )
+
+        super().__init__(None, **kwargs)
 
     def _recv_internal(self, timeout):
         pass
