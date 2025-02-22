09380dcf7d6eaa2e6c10a6c3f6309a8d5cc89c44
Peter Kessen <p.kessen@kessen-peter.de>
2025-02-22 19:56:42 +0100

    Add basic receive function

--- can/interfaces/rmcan/rmcan.py
+++ can/interfaces/rmcan/rmcan.py
@@ -19,6 +19,42 @@ class RmcanFrame(object):
         self.command = command
         self.data = data
 
+    @classmethod
+    def from_queue(cls, queue):
+        while True:
+            while True:
+                c = queue[0]
+                if c == 0x43:
+                    log.debug("Found SOF")
+                    break
+            d_len = queue[1]
+            # Frame length = SOF + len + data_len + Checksum + EOF
+            f_len = d_len + 4
+            if queue.length() < f_len:
+                log.debug("Incomplete frame")
+                raise Exception()
+            eof = data[f_len-1]
+            if eof != 0x0D:
+                log.warning("EOF not correct. Skipping one byte.")
+                queue.popleft()
+                break
+
+            cs = 0
+            for i in data[2:f_len-3]:
+                cs = cs ^ i
+            if cs != data[f_len-2]:
+                log.warning("Checksum error. Skipping one byte.")
+                queue.popleft()
+                break
+
+            cmd = queue[2]
+            data = queue[3:f_len-3]
+            for _ in range(f_len):
+                queue.pop()
+
+            frame = RmcanFrame(cmd, data)
+            return frame
+
     def to_raw(self):
         b = bytearray([0x43, self.command])
         b.append(len(self.data))
@@ -43,7 +79,7 @@ class RmcanBus(can.BusABC):
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.__message_buffer = deque()
-        self.__receive_buffer = ""  # i know string is not the most efficient here
+        self.__receive_buffer = bytearray()
 
         self.__socket.connect((self.__host, self.__port))
 
@@ -54,7 +90,33 @@ class RmcanBus(can.BusABC):
         super().__init__(None, **kwargs)
 
     def _recv_internal(self, timeout):
-        pass
+        if len(self.__message_buffer) != 0:
+            can_message = self.__message_buffer.popleft()
+            return can_message, False
+
+        try:
+            self.__receive_buffer += self.__socket.recv(1024)
+            if self.__tcp_tune:
+                self.__socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_QUICKACK, 1)
+
+            while True:
+                frame = RmcanFrame.from_queue(self.__receive_buffer)
+                if frame.command == 0x00:
+                    log.debug("Received standard frame")
+                    arbit_id = struct.unpack(">H", frame.data[0:1])
+                    msg = can.Message(
+                            arbitration_id=arbit_id,
+                            is_extended_frame=False,
+                            data=frame.data[2:],
+                            )
+                    return None, False
+                elif frame.command == 0x02:
+                    log.debug("Received extended frame")
+                else:
+                    log.warning("Received unsupported frame with command %s",
+                                (frame.command))
+        except Exception:
+            return None, False
 
     def send(self, msg, timeout=None):
         """Transmit a message to the CAN bus.
